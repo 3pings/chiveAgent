6957f9de8ac36a4f0680307d83b358576134b216
jbarksdale@threepings.com <jbarksdale@threepings.com>
2017-11-03 23:07:56 -0400

    Completed API pull of temperature data

--- main/main.go
+++ main/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/3pings/acigo/aci"
 	"log"
 	"os"
-
-	"github.com/udhos/acigo/aci"
+	"time"
 )
 
 func main() {
 
+	type nodeData struct {
+		temp string
+	}
+
+	nodeInfo := make(map[string]nodeData)
+
+	// Get environment variables for APIC login
 	debug := os.Getenv("DEBUG") != ""
 
 	a, errLogin := login(debug)
@@ -29,14 +36,59 @@ func main() {
 	}
 
 	for _, n := range nodes {
-		//name := n["name"]
-		dn := n["dn"]
-		role := n["role"]
-		serial := n["serial"]
-		fmt.Println(dn, role, serial)
-	}
-}
 
+		nodeDetails, errList := a.GetTemp(n["dn"].(string))
+		if errList != nil {
+			log.Printf("could not list node details: %v", errList)
+			return
+		}
+
+		for _, d := range nodeDetails {
+			name := n["name"].(string)
+			tempMax := d["currentMax"].(string)
+			nodeRole := n["role"].(string)
+
+			// At this time we do not want the controller info
+			if nodeRole != "controller" {
+				nodeInfo[name] = nodeData{tempMax}
+			}
+		}
+	}
+	//Printing today need to add api call
+	fmt.Println(nodeInfo)
+
+	// Loop through infinitely every X number of seconds
+	for {
+		time.Sleep(60 * time.Second)
+		errRefresh := a.Refresh()
+		if errRefresh != nil {
+			log.Printf("refresh %d/%d error: %v", errRefresh)
+			os.Exit(3)
+		}
+
+		for _, n := range nodes {
+
+			nodeDetails, errList := a.GetTemp(n["dn"].(string))
+			if errList != nil {
+				log.Printf("could not list node details: %v", errList)
+				return
+			}
+
+			for _, d := range nodeDetails {
+				name := n["name"].(string)
+				tempMax := d["currentMax"].(string)
+				nodeRole := n["role"].(string)
+
+				// At this time we do not want the controller info
+				if nodeRole != "controller" {
+					nodeInfo[name] = nodeData{tempMax}
+				}
+			}
+		}
+	}
+	//Printing today need to add api call
+	fmt.Println(nodeInfo)
+}
 
 func login(debug bool) (*aci.Client, error) {
 
